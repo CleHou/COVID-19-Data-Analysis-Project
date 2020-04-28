9a5df8de916aef932d2002faf906496158ecafb1
CleHou <59105119+CleHou@users.noreply.github.com>
2020-04-28 22:34:31 +0200

    Update 32 - Visualisation of RMSE.py

--- 03-Prediction/32 - Visualisation of RMSE.py
+++ 03-Prediction/32 - Visualisation of RMSE.py
@@ -229,8 +229,8 @@ def create_values_C (list_df, country, list_a, list_b, list_c):
 #Creation des DB
 current = os.path.dirname(os.path.realpath(__file__))
 
-#list_df, to_replace = import_df_from_I()
-list_df, to_replace = import_df_from_xlsx(current)
+list_df, to_replace = import_df_from_I() #Data import from the Internet
+#list_df, to_replace = import_df_from_xlsx(current) #Data import from local file if already downloaded
 save_df (list_df, to_replace)
 
 list_df= clean_up(list_df)
