ec57a7d64994db7d5ebd493897d9f89b468b5d7d
CleHou <59105119+CleHou@users.noreply.github.com>
2021-03-01 10:24:33 +0100

    Major update
    Code new ready to run but:
    - Maps not working
    + diverse improvement needed

--- src/gen_fct/file_fct.py
+++ src/gen_fct/file_fct.py
@@ -8,6 +8,11 @@ Created on Sat Jan  9 15:56:01 2021
 import os 
 import numpy
 import pandas
+import datetime
+import holoviews as hv
+import matplotlib.image as image
+
+from gen_fct import df_fct
 
 def list_dir_files (file_path):
     file_path = os.path.normcase(file_path)
@@ -37,3 +42,76 @@ def creation_folder (root, paths):
             os.makedirs(directory)
     list_return = [root + x for x in paths]      
     return list_return
+
+def save_fig (an_object, name, date, **kwargs):
+    db_file, db_file_date = df_fct.write_db_files (name, date, kwargs)
+
+    date_str = date.strftime("%Y-%m-%d")
+    year = date.strftime("%Y")
+    month = date.strftime("%m - %B")
+    
+    if db_file.loc[name, 'add_date']:
+        file_dir = f"{get_parent_dir(2)}/{db_file.loc[name, 'local_path']}/{year}/{month}"
+        file_dir_prev = f"{get_parent_dir(2)}/{db_file.loc[name, 'local_path_prev']}/{year}/{month}"
+        file_name = f"{db_file.loc[name, 'pref']}{date_str}{db_file.loc[name, 'suf']}"
+
+    else:
+        file_dir = f"{get_parent_dir(2)}/{db_file.loc[name, 'local_path']}"
+        file_dir_prev = f"{get_parent_dir(2)}/{db_file.loc[name, 'local_path_prev']}"
+        file_name = f"{db_file.loc[name, 'pref']}{db_file.loc[name, 'suf']}"
+
+    root = get_parent_dir(2, 'reports')
+    creation_folder ('', [file_dir, file_dir_prev])
+
+    if db_file.loc[name, 'type_file'] == 'Graph':
+        root_img = get_parent_dir(2, 'data')
+        logo = image.imread(f'{root_img}/logo/Logo2_200px.png')
+        an_object.figimage(logo, 30, 20, zorder=3)
+
+        an_object.savefig(os.path.normcase(f'{file_dir}/{file_name}.pdf'), format='pdf', dpi=200)
+        an_object.savefig(os.path.normcase(f'{file_dir_prev}/{file_name}_preview.png'), format='png', dpi=300)
+        #fig.savefig(os.path.normcase(f'{file_dir}/{file_name}.svg'), format='svg', dpi=300)
+        print(f'Graph {name} saved')
+
+    elif db_file.loc[name, 'type_file'] == 'Map':
+        print('\n\nSaving Map...\r')
+        renderer = hv.renderer('bokeh')
+        renderer.save(an_object, os.path.normcase(f'{file_dir}/{file_name}'))
+        print(f'Map {name} saved')
+
+    elif db_file.loc[name, 'type_file'] == 'MapPrev':
+        hv.save(an_object, os.path.normcase(f'{file_dir}/{file_name}.png'))
+        print(f'Map Preview {name} saved')
+
+    df_fct.save_db_files (db_file, db_file_date)
+
+def save_map (Map, name, date, **kwargs):
+    db_file, db_file_date = df_fct.write_db_files (name, date, kwargs)
+
+    date_str = date.strftime("%d-%m-%Y")
+    year = date.strftime("%Y")
+    month = date.strftime("%m - %B")
+    
+    if db_file.loc[name, 'add_date']:
+        file_dir = f"{get_parent_dir(2)}/{db_file.loc[name, 'local_path']}/{year}/{month}"
+        file_dir_prev = f"{get_parent_dir(2)}/{db_file.loc[name, 'local_path_prev']}/{year}/{month}"
+
+    else:
+        file_dir = f"{get_parent_dir(2)}/{db_file.loc[name, 'local_path']}"
+        file_dir_prev = f"{get_parent_dir(2)}/{db_file.loc[name, 'local_path_prev']}"
+
+    root = get_parent_dir(2, 'reports')
+    creation_folder ('', [file_dir, file_dir_prev])
+    file_name = f"{db_file.loc[name, 'pref']}{date_str}{db_file.loc[name, 'suf']}"
+
+    print('\n\nSaving Map...')
+    renderer = hv.renderer('bokeh')
+    renderer.save(Map, os.path.normcase(f'{file_dir}/{file_name}'))
+    print(f'Map {name} saved')
+    
+    df_fct.save_db_files (db_file, db_file_date)
+
+
+
+
+
